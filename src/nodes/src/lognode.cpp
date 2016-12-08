ff9758f62828f8997c74708c280cd58c97a931cf
Svendsen92 <ssven16@student.aau.dk>
2016-12-08 15:35:06 +0100

    Update lognode.cpp

--- src/nodes/src/lognode.cpp
+++ src/nodes/src/lognode.cpp
@@ -17,6 +17,7 @@
 
 using namespace std;
 
+//////Declaring Global variables//////
 float quatx;
 float quaty;
 float quatz;
@@ -24,6 +25,9 @@ float quatw;
 string message;
 ofstream myfile ("Data_Saved.txt");
 
+////////////Functions//////////////
+
+//This function converts a float into a string
 string float_to_sting(float val){
   string s;
   stringstream ss (stringstream::in | stringstream::out);
@@ -32,11 +36,16 @@ string float_to_sting(float val){
   return s;
 }
 
+//This function prints the text "The operation has been received 
+//for the coordinates: " and the chosen coordinates to a txt.file
 void save_coord_func(float x, float y, ofstream &myfile){
   myfile << "The operation has been received for the coordinates: (" 
          << float_to_sting(x) << ") (" << float_to_sting(y) << ")" << endl;
 }
 
+//This function receives the coordinates from the interface and calls on the "save_coord_func"
+//while printing "The operation has been received for the coordinates: " and the coordinates 
+//in a ros terminal
 void coord_func(const geometry_msgs::Point::ConstPtr& point_msg) {
   cout << endl;
   ROS_INFO("The operation has been received for the coordinates: x: %f y: %f", 
@@ -45,10 +54,15 @@ void coord_func(const geometry_msgs::Point::ConstPtr& point_msg) {
   quaty = point_msg->y;
   save_coord_func(quatx, quaty, myfile);
 }
+
+//This function prints the text "The 'the task' operation has been received!" 
+//to a txt.file
 void save_task_func(string x, ofstream &myfile){
   myfile << "The " << x << " operation has been received! " << endl;
 }
 
+//This function receives the "task" text from the interface and calls on the "save_task_func"
+//while printing the "task" text in a ros terminal
 void task_func(const std_msgs::String::ConstPtr& task_msg) {
   cout << endl;
   ROS_INFO("The '%s' operation has been received! ", task_msg->data.c_str());
@@ -56,10 +70,14 @@ void task_func(const std_msgs::String::ConstPtr& task_msg) {
   save_task_func(message, myfile);
 }
 
+//This function prints the "status" text 
+//to a txt.file
 void save_status_func(string x, ofstream &myfile){
   myfile << x << endl;
 }
 
+//This function receives the status from the interface and calls on the "save_status_func"
+//while printing the "status" text in a ros terminal
 void status_func(const std_msgs::String::ConstPtr& status_msg){
   cout << endl;
   ROS_INFO("%s", status_msg->data.c_str());
@@ -67,12 +85,17 @@ void status_func(const std_msgs::String::ConstPtr& status_msg){
   save_status_func(message, myfile);
 }
 
+//This function prints the text "The operation has been received 
+//for the coordinates" and the chosen coordinates to a txt.file
 void save_quat_func(float x, float y, float z, float w, ofstream &myfile){
   myfile << "The operation has been received for the coordinates: ("
    << float_to_sting(x) << ") (" << float_to_sting(y)
    << ") (" << float_to_sting(z) << ") (" << float_to_sting(w) << ")" << endl;     
 }
 
+//This function receives the coordinates from the interface and calls on the "save_quat_func"
+//while printing "The operation has been received for the coordinates: " and the coordinates 
+//in a ros terminal
 void quat_func(const geometry_msgs::Quaternion::ConstPtr& quaternion_msg){
   cout << endl;
   ROS_INFO("The operation has been received for the coordinates: x: %f y: %f z: %f w: %f",
