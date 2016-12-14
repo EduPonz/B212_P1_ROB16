55b83f504e0ffdad137f09a91e3ad095dea26ccb
dmicha16 <dmicha16@student.aau.dk>
2016-12-14 18:13:50 +0100

    added comments

--- src/nodes/src/checklist.cpp
+++ src/nodes/src/checklist.cpp
@@ -29,7 +29,8 @@ class Checklist
     ros::Subscriber task_sub;
     ros::Publisher check_pub;
     std::string task_temp;
-
+    // Callback function for the /task subscription, after this function calls on the next
+    // operation steps that are all functions themselfs (_stepOne)
     void _taskFunc(const std_msgs::String::ConstPtr& task_msg) {
       task_temp = task_msg->data;
       if(task_temp == "Checklist") {
@@ -39,7 +40,7 @@ class Checklist
         _stepFour(check_pub);
       }
     }
-
+    // Publishing function to publish the status of the checklist operation
     void _publishStatus (string s, ros::Publisher check_pub) {
       string status = s;
       std_msgs::String status_msg;
@@ -48,7 +49,7 @@ class Checklist
       
       ros::spinOnce(); 
     }
-
+    // Step one of the checklist operation
     void _stepOne(ros::Publisher check_pub) {
 
       MoveBaseClient ac("move_base", true);
@@ -58,15 +59,17 @@ class Checklist
         s = "Waiting for the move_base action server to come up";
         _publishStatus(s, check_pub);
       }
-
+      
       move_base_msgs::MoveBaseGoal goal;
+      // In this case base_link was use as the frame ID so the robot does not use the map generated
+      // and can perform the operations in adequte space
       goal.target_pose.header.frame_id = "base_link";
       goal.target_pose.header.stamp = ros::Time::now();
-
+      // Target positions from the starting point
       goal.target_pose.pose.position.x = 2.0;
       goal.target_pose.pose.position.y = 0.0;
       goal.target_pose.pose.position.z = 0.0;
-
+      // Orienation as quaternion coordinates
       goal.target_pose.pose.orientation.x = 0.0;
       goal.target_pose.pose.orientation.y = 0.0;
       goal.target_pose.pose.orientation.z = 0.0;
@@ -76,16 +79,16 @@ class Checklist
       _publishStatus(s, check_pub);
       ac.sendGoal(goal);
       ac.waitForResult();
-
+      // Publishing the success status of the operation
       if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         s = "Step 1 has been completed. Arrived to origin.";
         _publishStatus(s, check_pub);
-      }else {
+      }else { // Publishing the failure status of the operation
         s = "Step 1 failed";
         _publishStatus(s, check_pub);
       }
     }
-
+    // Same function as before but using different coordinates
     void _stepTwo(ros::Publisher check_pub) {
 
       MoveBaseClient ac("move_base", true);
@@ -123,7 +126,7 @@ class Checklist
         _publishStatus(s, check_pub);
       }
     }
-
+    // Same function as before but using different coordinates
     void _stepThree(ros::Publisher check_pub) {
 
       MoveBaseClient ac("move_base", true);
@@ -161,7 +164,7 @@ class Checklist
         _publishStatus(s, check_pub);
       }
     }
-
+    // Same function as before but using different coordinates
     void _stepFour(ros::Publisher check_pub) {
       MoveBaseClient ac("move_base", true);
       string s;
@@ -200,6 +203,7 @@ class Checklist
     }
 
   public:
+  // Checklist class construction
     Checklist(ros::NodeHandle n) {
       ros::Subscriber task_sub = n.subscribe("task", 100, &Checklist::_taskFunc, this);
       check_pub = n.advertise<std_msgs::String>("checklist_status", 100);
