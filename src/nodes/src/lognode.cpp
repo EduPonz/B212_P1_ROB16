ba3f18ce7bb09b8edbe0e9b91d78e3bfe11d108d
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2016-12-17 15:20:21 +0100

    Update lognode.cpp

--- src/nodes/src/lognode.cpp
+++ src/nodes/src/lognode.cpp
@@ -2,8 +2,6 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/PointStamped.h"
-//#include "geometry_msgs/point.h"
-//#include "geometry_msgs/pointStamped.h"
 #include <ros/console.h>
 #include <iostream>
 #include <string.h>
@@ -26,7 +24,6 @@ class LogNode{
       float pointy;
       float pointz;
       string message;
-
       // This function converts a float into a string
       string _floatToString(float val){
         string s;
@@ -35,47 +32,41 @@ class LogNode{
         s = ss.str();
         return s;
       }
-
-      //This function writes the text "The 'the task' operation has been received!" 
-      //to a .txt file
+      // This function writes the text "The 'the task' operation has been received!" 
+      // to a .txt file
       void _saveTaskFunc(string x, ofstream &myfile) {
         myfile << "The '" << x << "' operation has been received! " << endl;
       }
-
-      //This function receives the "task" text from the interface and calls on the "_saveTaskFunc" function
-      //while printing the "task" text to the screen
+      // This function receives the "task" text from the interface and calls on the "_saveTaskFunc" function
+      // while printing the "task" text to the screen
       void _taskFunc(const std_msgs::String::ConstPtr& task_msg) {
         cout << " The '" << task_msg->data.c_str() <<"' operation has been received!" << endl;
         message = task_msg->data.c_str();
         _saveTaskFunc(message, myfile);
       }
-
-      //This function writes the "status" text 
-      //to a .txt file
+      // This function writes the "status" text 
+      // to a .txt file
       void _saveStatusFunc(string x, ofstream &myfile){
         myfile << x << endl;
       }
-
-      //This function receives the status from the interface and calls on the "_saveStatusFunc" function
-      //while printing the "status" text to the screen
+      // This function receives the status from the interface and calls on the "_saveStatusFunc" function
+      // while printing the "status" text to the screen
       void _statusFunc(const std_msgs::String::ConstPtr& status_msg){
         cout << endl;
         cout << " " << status_msg->data.c_str() << endl;
         message = status_msg->data.c_str();
         _saveStatusFunc(message, myfile);
       }
-
-      //This function writes the text "The operation has been received 
-      //for the coordinates" and the chosen coordinates to a .txt file
+      // This function writes the text "The operation has been received 
+      // for the coordinates" and the chosen coordinates to a .txt file
       void _savePointFunc(float x, float y, float z, ofstream &myfile){
         myfile << "The operation has been received for the coordinates: ("
                << _floatToString(x) << ") (" << _floatToString(y)
                << ") (" << _floatToString(z) << ") " << endl;     
       }
-
-      //This function receives the coordinates from the interface and calls on the "_savepointFunc" function
-      //while printing "The operation has been received for the coordinates: " and the coordinates 
-      //to the screen
+      // This function receives the coordinates from the interface and calls on the "_savepointFunc" function
+      // while printing "The operation has been received for the coordinates: " and the coordinates 
+      // to the screen
       void _pointFunc(const geometry_msgs::Point::ConstPtr& point_msg){
         cout << " The operation has been received for the coordinates: " << endl << " ("
              << point_msg->x << ", " 
@@ -100,7 +91,6 @@ class LogNode{
 
 
 int main(int argc, char **argv) {
-
   ros::init(argc, argv, "lognode");
   ros::NodeHandle myNodeHandle;
   LogNode myLogNode (myNodeHandle);
